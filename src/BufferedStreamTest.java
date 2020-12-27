754f54207a774e482b54f1622b655a857fd39a07
kimjiyun <didany15@dongguk.ac.kr>
2020-12-27 23:23:42 +0900

    flush() 구문 사용

--- src/BufferedStreamTest.java
+++ src/BufferedStreamTest.java
@@ -1,78 +1,51 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
 
 
 public class BufferedStreamTest {
 
+    /**
+     * @param args
+     * @throws FileNotFoundException
+     */
     public static void main(String[] args) {
-
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-
-        FileOutputStream fos = null;
-        BufferedOutputStream bos = null;
-
-        Date d= null;
-
-        try{
-
-            // FileInputStream 으로 "StreamFile.txt" 파일의 객체를 생성 후
-            // BufferedInputStream 객체를 생성한다.
-            fis = new FileInputStream("StreamFile.txt");
-            bis = new BufferedInputStream(fis);
-
-            // FileOutputStream 으로 출력할 파일 "copyFile.txt" 객체 생성 후
-            // BufferedOutputStream 객체를 생성한다.
-            fos = new FileOutputStream("copyFile.txt");
-            bos = new BufferedOutputStream(fos);
-
-            int i =0;
-            d= new Date();
-            long start = d.getTime();
-
-
-            // "StreamFile.txt" 에서 1바이트씩 읽어서 버퍼에 담는다.
-            while((i=bis.read())!=-1){
-
-                // 1바이트씩 출력버퍼에 담는다.
-                bos.write(i);
+        BufferedInputStream in = null;
+        BufferedOutputStream out = null;
+        int c;
+        try {
+            out = new BufferedOutputStream(new FileOutputStream("/Users/kimjiyun/IdeaProjects/BufferedStreamTest/StreamFile.txt"));
+            for(int i=0;i<10;i++) {
+                out.write(i);
             }
-
-            d= new Date();
-            long end = d.getTime();
-            System.out.println("복사 시간 : " + (end-start));
-
-        }catch(Exception e){
+            // flush() : 버퍼를 수동으로 비우기 위해 사용하는 메서드
+            out.flush(); //버퍼의 내용을 파일에 쓴다.
+            in = new BufferedInputStream(new FileInputStream("/Users/kimjiyun/IdeaProjects/BufferedStreamTest/StreamFile.txt"));
+            while((c = in.read()) != -1) {
+                System.out.print(c + " ");
+            }
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally{
-
-            // 마지막에 FileInputStream / FileOutputStream을 닫아준다.
-            // BufferedInputStream / BufferedOutputStream 도 닫아준다.
-            if(bis != null)
-                try{
-                    bis.close();
-                }catch(IOException e){}
-            if(fis != null)
-                try{
-                    fis.close();
-                }catch(IOException e){}
-
-            // BufferedOutputStream 이 close() 되면서 버퍼의 내용을 출력한다.
-            if(bos != null)
-                try{
-                    bos.close();
-                }catch(IOException e){}
-            if(fos != null)
-                try{
-                    fos.close();
-                }catch(IOException e){}
-
+        }finally {
+            if(in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
     }
-
 }
